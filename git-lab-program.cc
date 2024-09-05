36eb28bcbb736722883eaee2393368118d98148a
Ayesha Akbar <159517713+ayeshaakbr@users.noreply.github.com>
2024-09-05 17:00:25 -0400

    Add files via upload

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Ayesha AKbar
+ *        Date: SEp 5 2024
+ * Description: CS2400
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
